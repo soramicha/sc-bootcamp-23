66108479a7a66e18cf8b659eaceaf3256e8350c3
soramicha <schang7jcu@gmail.com>
2023-12-21 19:17:02 -0800

    Milestone 4

--- bootcamp-milestone-2/src/helpers/db.ts
+++ bootcamp-milestone-2/src/helpers/db.ts
@@ -1,7 +1,7 @@
 // db.ts
 import mongoose from "mongoose";
 
-const url: string = process.env.MONGO_URI as string;
+const url: string = "abscs" //process.env.MONGO_URI as string;
 console.log(url + "is the url, sophia");
 let connection: typeof mongoose;
 
